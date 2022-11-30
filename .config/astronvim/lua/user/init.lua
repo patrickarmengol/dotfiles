3633a19e2d6a2548c2e6f0c056b538c9a6f8360e
Patrick Armengol <patrickarmengol@protonmail.com>
2022-11-30 08:34:07 +0000

    removed default theme lines

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -28,16 +28,6 @@ local config = {
   -- Set colorscheme to use
   colorscheme = "catppuccin-mocha",
 
-  -- Add highlight groups in any theme
-  highlights = {
-    -- init = { -- this table overrides highlights in all themes
-    --   Normal = { bg = "#000000" },
-    -- }
-    -- duskfox = { -- a table of overrides/changes to the duskfox theme
-    --   Normal = { bg = "#000000" },
-    -- },
-  },
-
   -- set vim options here (vim.<first_key>.<second_key> = value)
   options = {
     opt = {
@@ -82,50 +72,6 @@ local config = {
     "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
     "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
     "    ██   ████   ████   ██ ██      ██",
-  },
-
-  -- Default theme configuration
-  default_theme = {
-    -- Modify the color palette for the default theme
-    colors = {
-      fg = "#abb2bf",
-      bg = "#1e222a",
-    },
-    highlights = function(hl) -- or a function that returns a new table of colors to set
-      local C = require "default_theme.colors"
-
-      hl.Normal = { fg = C.fg, bg = C.bg }
-
-      -- New approach instead of diagnostic_style
-      hl.DiagnosticError.italic = true
-      hl.DiagnosticHint.italic = true
-      hl.DiagnosticInfo.italic = true
-      hl.DiagnosticWarn.italic = true
-
-      return hl
-    end,
-    -- enable or disable highlighting for extra plugins
-    plugins = {
-      aerial = true,
-      beacon = false,
-      bufferline = true,
-      cmp = true,
-      dashboard = true,
-      highlighturl = true,
-      hop = false,
-      indent_blankline = true,
-      lightspeed = false,
-      ["neo-tree"] = true,
-      notify = true,
-      ["nvim-tree"] = false,
-      ["nvim-web-devicons"] = true,
-      rainbow = true,
-      symbols_outline = false,
-      telescope = true,
-      treesitter = true,
-      vimwiki = false,
-      ["which-key"] = true,
-    },
   },
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
@@ -224,6 +170,7 @@ local config = {
       -- Add plugins, the packer syntax without the "use"
       -- { "andweeb/presence.nvim" },
       { "catppuccin/nvim", as = "catppuccin" }
+      -- {
       --   "ray-x/lsp_signature.nvim",
       --   event = "BufRead",
       --   config = function()
