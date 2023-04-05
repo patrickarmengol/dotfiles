ede70253d809f585b44e2cceb36b270d82a47ab6
Patrick Armengol <patrickarmengol@protonmail.com>
2023-04-05 12:31:26 +0000

    add astronvim

---
+++ .config/astronvim/lua/user/plugins/community.lua
@@ -0,0 +1,17 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of imporing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+    
+  -- colorschemes
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  
+  -- languages
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  
+}
